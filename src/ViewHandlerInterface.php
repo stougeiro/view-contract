efd59b4f3db08592ffa030867ae594baa15643fc
STougeiro <sidneytougeiro@gmail.com>
2024-09-17 22:12:56 -0300

    Initial commit

---
+++ src/ViewHandlerInterface.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+    namespace STDW\View\Contract;
+
+
+    interface ViewHandlerInterface extends ViewInterface
+    { }
---
+++ src/ViewInterface.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+    namespace STDW\View\Contract;
+
+
+    interface ViewInterface
+    {
+        public function compile(string $file, array $data = []): string;
+
+        public function render(string $file, array $data = []): void;
+    }
