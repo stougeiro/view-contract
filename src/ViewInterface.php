63e79d57806043668c8416c617ad352e9cb02dcc
STougeiro <sidneytougeiro@gmail.com>
2024-09-23 00:52:16 -0300

    Adding compose function

--- src/ViewInterface.php
+++ src/ViewInterface.php
@@ -7,6 +7,8 @@
     {
         public function setStorage(string $name, string $path): void;
 
+        public function compose(array $data): void;
+
         public function compile(string $file, array $data = []): string;
 
         public function render(string $file, array $data = []): void;
